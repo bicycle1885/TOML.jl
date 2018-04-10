16ed8bf6af0f9d692276320eb9deb8eeb8c86773
Kenta Sato <bicycle1885@gmail.com>
2018-04-10 23:03:17 +0900

    remove debug utility

--- src/TOML.jl
+++ src/TOML.jl
@@ -8,27 +8,5 @@ include("token.jl")
 include("tokenizer.jl")
 include("parser.jl")
 
-function debug(str::AbstractString)
-    tokens = Token[]
-    reader = StreamReader(IOBuffer(str))
-    lasttoken = nothing
-    while true
-        try
-            token = parsetoken(reader)
-            #println(token)
-            if token.kind == :eof
-                break
-            else
-                push!(tokens, token)
-            end
-            lasttoken = token
-        catch
-            @show lasttoken
-            rethrow()
-        end
-    end
-    return tokens
-end
-
 end # module
 
